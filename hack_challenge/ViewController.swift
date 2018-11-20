30dad961e25e693647da5bb29af6106dfb882bcd
Mathew Scullin <mathewscullin@mathews-macbook-pro.local>
2018-11-20 15:59:12 -0500

    Initial Commit

---
+++ hack_challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hack_challenge
+//
+//  Created by Mathew Scullin on 11/20/18.
+//  Copyright © 2018 Mathew Scullin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
